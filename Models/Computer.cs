150906a19cc9cb62a7b9a008b64b59edfcb054ab
steve schnell <steveschnell5@gmail.com>
2023-08-25 16:39:21 -0500

    Namespace

---
+++ Models/Computer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloWorld.Models
+{
+    public class Computer
+    {
+        public string Motherboard { get; set; } = "";
+
+        public int CPUCores { get; set; }
+
+        public bool HasWiFi { get; set; }
+
+        public bool HasLTE { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string VideoCard { get; set; } = "";
+
+        public Computer()
+        {
+        }
+    }
+}
--- Program2.cs
+++ Program2.cs
@@ -3,28 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloWorld.Models;
 
 namespace HelloWorld
 {
-    public class Computer
-    {
-        public string Motherboard { get; set; }
-
-        public int CPUCores { get; set; }
-
-        public bool HasWiFi { get; set; }
-
-        public bool HasLTE { get; set; }
-
-        public DateTime ReleaseDate { get; set; }
-
-        public decimal Price { get; set; }
-
-        public string VideoCard { get; set; }
-
-        }
-    }
-
     internal class Program2
     {
         static void Main(string[] args)
